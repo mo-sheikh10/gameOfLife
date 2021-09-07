7dc3fa4e6ca6240ceb97804582a181781962ed9b
Mohamed Sheikh <mohamed.sheikh@cazoo.co.uk>
2021-09-07 11:20:14 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/gameOfLife.ts
+++ src/main/gameOfLife.ts
@@ -2,39 +2,38 @@ interface Position {
     positionX: number,
     positionY: number
 }
-export class GameOfLife {
-    private grid: number[][]
 
-    constructor() {
-        this.grid = [
-            [0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]            
-        ]
+type Grid = number[][]
+export class GameOfLife {
+    private grid: Grid
+
+    constructor(grid: Grid) {
+        this.grid = grid
     }
 
-    shouldLive(position: Position){
-        let count = 0
-        this.grid[position.positionX, position.positionY]
-
+    shoudLive(position: Position){
+        let liveNeighbours = 0
         for (let i = (position.positionY - 1); i <= (position.positionY + 1); i++) {
             for (let j = (position.positionX - 1); j <= (position.positionX + 1); j++) {
                 if (i == position.positionY && j == position.positionX){
                     continue
                 }
                 if (this.grid[i][j] == 1){
-                    count++
+                    liveNeighbours++
                 }
             }
         }
-        if (count < 2) {
+
+        if (liveNeighbours < 2) {
             return false
         }
-        return true
+        if(liveNeighbours === 3){
+            return true
+        }
+
+
     }
-    
+
 }
 // (i-1,j-1) (i-1,j) (i-1,j+1)
 // (i  ,j-1)    1    (i  ,j+1)
--- src/test/gameOfLife.test.ts
+++ src/test/gameOfLife.test.ts
@@ -1,11 +1,30 @@
 import {GameOfLife} from "../main/gameOfLife";
 
 describe('Game Of Life', () => {
-    describe("Given we have a live cell with fewer than two live neighbours", ()=>{
-        it("should die",()=>{
-            const gameOfLife = new GameOfLife()
-            const cell = gameOfLife.shouldLive({positionX: 1, positionY: 2})
+    describe("Given we have a live cell ", ()=>{
+        it("should die if fewer than two live neighbours",()=>{
+           let grid =  [
+                [0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ]
+            const gameOfLife = new GameOfLife(grid)
+            const cell = gameOfLife.shoudLive({positionX: 1, positionY: 1})
             expect(cell).toBe(false)
+        })
+        it("should live if three live neighbours",()=>{
+            let grid =  [
+                [0, 1, 1, 0, 0],
+                [0, 1, 0, 0, 0],
+                [0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ]
+            const gameOfLife = new GameOfLife(grid)
+            const cell = gameOfLife.shoudLive({positionX: 1, positionY: 1})
+            expect(cell).toBe(true)
         })
 
     })
